d3ce03e8d7b8cd78875ed3a93e235774e61395e7
José Grilo <josegrilo.q1b@gmail.com>
2022-05-18 18:41:53 +0100

    Animação resolvida

--- SimulaçãoEModelação/Ecossistema/Ecossis.py
+++ SimulaçãoEModelação/Ecossistema/Ecossis.py
@@ -306,7 +306,7 @@ def circleOfLife(nx, ny, nIterations):
     hHerb = np.zeros((nIterations + 1, 3), dtype = float)
     hCarn = np.zeros((nIterations + 1, 3), dtype = float)
     ecossistemas = []
-    ecossistemas.append(tudo[0])
+    ecossistemas.append(copy.deepcopy(tudo[0]))
     gridInt = np.zeros((nx, ny), dtype = float)
     
     nPlant[0] = len(tudo[3])/(nx * ny) * 100
@@ -342,7 +342,7 @@ def circleOfLife(nx, ny, nIterations):
             
     for i in range(nIterations):
         iteration(tudo)
-        ecossistemas.append(np.copy(tudo[0]))
+        ecossistemas.append(copy.deepcopy(tudo[0]))
         nPlant[i + 1] = len(tudo[3])/(nx * ny) * 100
         nHerb[i + 1] = len(tudo[1])/(nx * ny) * 100
         nCarn[i + 1] = len(tudo[2])/(nx * ny) * 100
@@ -375,15 +375,16 @@ def circleOfLife(nx, ny, nIterations):
         for j in range(nx):
             for k in range(ny):
                 gridInt[j, k] = ecossistemas[i][j, k].type
-        ecossistemas.append(np.copy(gridInt))
+        ecossistemas.append(copy.deepcopy(gridInt))
         
+
     tudo2 = initGrid(nx, ny, 9, 3, 0)
     nPlant2 = np.zeros(nIterations + 1, dtype = float) 
     nHerb2 = np.zeros(nIterations + 1, dtype = float) 
     hPlant2 = np.zeros((nIterations + 1, 3), dtype = float)
     hHerb2 = np.zeros((nIterations + 1, 3), dtype = float)
     ecossistemas2 = []
-    ecossistemas2.append(tudo2[0])
+    ecossistemas2.append(copy.deepcopy(tudo2[0]))
     
     nPlant2[0] = len(tudo2[3])/(nx * ny) * 100
     nHerb2[0] = len(tudo2[1])/(nx * ny) * 100
@@ -408,7 +409,7 @@ def circleOfLife(nx, ny, nIterations):
     
     for i in range(nIterations):
         iteration(tudo2)
-        ecossistemas2.append(tudo2[0])
+        ecossistemas2.append(copy.deepcopy(tudo2[0]))
         nPlant2[i + 1] = len(tudo2[3])/(nx * ny) * 100
         nHerb2[i + 1] = len(tudo2[1])/(nx * ny) * 100
         for j in range(len(tudo2[3])):
@@ -432,94 +433,80 @@ def circleOfLife(nx, ny, nIterations):
         for j in range(nx):
             for k in range(ny):
                 gridInt[j, k] = ecossistemas2[i][j, k].type
-        grid2 = np.copy(gridInt)
-        ecossistemas2.append(grid2)
+        ecossistemas2.append(copy.deepcopy(gridInt))
+
     
     #PLOTS
-# =============================================================================
-#     fig = plt.figure()
-#     axS = fig.add_subplot(4, 2, 1)
-#     axS.plot(nPlant, 'b-', label = "Plantas")
-#     axS.plot(nHerb, 'g-', label = "Herbívoros")
-#     axS.plot(nCarn, 'r-', label = "Carnívoros")
-#     axS.set_title('Simulação c/ Carnívoros')
-#     axS.set_ylabel('%')
-#     axS.set_xlabel('Número da Iteração')
-#     plt.legend()
-#     axS2 = fig.add_subplot(4, 2, 2)
-#     axS2.plot(nPlant2, 'b-', label = "Plantas")
-#     axS2.plot(nHerb2, 'g-', label = "Herbívoros")
-#     axS2.set_title('Simulação s/ Carnívoros')
-#     axS2.set_ylabel('%')
-#     axS2.set_xlabel('Número da Iteração')
-#     plt.legend()
-#     axS3 = fig.add_subplot(4, 2, 3)
-#     axS3.plot(hPlant[:, 0], 'b-', label = "Fraco")
-#     axS3.plot(hPlant[:, 1], 'g-', label = "Médio")
-#     axS3.plot(hPlant[:, 2], 'r-', label = "Forte")
-#     axS3.set_title('População de Plantas por Nível')
-#     axS3.set_ylabel('%')
-#     axS3.set_xlabel('Número da Iteração')
-#     plt.legend()
-#     axS4 = fig.add_subplot(4, 2, 5)
-#     axS4.plot(hHerb[:, 0], 'b-', label = "Fraco")
-#     axS4.plot(hHerb[:, 1], 'g-', label = "Médio")
-#     axS4.plot(hHerb[:, 2], 'r-', label = "Forte")
-#     axS4.set_title('População de Herbívoros por Nível')
-#     axS4.set_ylabel('%')
-#     axS4.set_xlabel('Número da Iteração')
-#     plt.legend()
-#     axS5 = fig.add_subplot(4, 2, 7)
-#     axS5.plot(hCarn[:, 0], 'b-', label = "Fraco")
-#     axS5.plot(hCarn[:, 1], 'g-', label = "Médio")
-#     axS5.plot(hCarn[:, 2], 'r-', label = "Forte")
-#     axS5.set_title('População de Carnívoros por Nível')
-#     axS5.set_ylabel('%')
-#     axS5.set_xlabel('Número da Iteração')
-#     plt.legend()
-#     axS6 = fig.add_subplot(4, 2, 4)
-#     axS6.plot(hPlant2[:, 0], 'b-', label = "Fraco")
-#     axS6.plot(hPlant2[:, 1], 'g-', label = "Médio")
-#     axS6.plot(hPlant2[:, 2], 'r-', label = "Forte")
-#     axS6.set_title('População de Plantas por Nível')
-#     axS6.set_ylabel('%')
-#     axS6.set_xlabel('Número da Iteração')
-#     plt.legend()
-#     axS7 = fig.add_subplot(4, 2, 6)
-#     axS7.plot(hHerb2[:, 0], 'b-', label = "Fraco")
-#     axS7.plot(hHerb2[:, 1], 'g-', label = "Médio")
-#     axS7.plot(hHerb2[:, 2], 'r-', label = "Forte")
-#     axS7.set_title('População de Herbívoros por Nível')
-#     axS7.set_ylabel('%')
-#     axS7.set_xlabel('Número da Iteração')
-#     plt.legend()
-#     fig.set_size_inches(12, 6)
-# =============================================================================
-    
-    
-    
-    
-   
-    
-# =============================================================================
-#     ax1 = fig1.add_subplot(1, 2, 1)
-#     hm1 = ax1.imshow(ecossistemas[260], aspect = 'auto', vmin = 0, vmax = 3)
-#     plt.colorbar(hm1)
-#     ax2 = fig1.add_subplot(1, 2, 2)
-# =============================================================================
+    fig = plt.figure()
+    axS = fig.add_subplot(4, 2, 1)
+    axS.plot(nPlant, 'b-', label = "Plantas")
+    axS.plot(nHerb, 'g-', label = "Herbívoros")
+    axS.plot(nCarn, 'r-', label = "Carnívoros")
+    axS.set_title('Simulação c/ Carnívoros')
+    axS.set_ylabel('%')
+    axS.set_xlabel('Número da Iteração')
+    plt.legend()
+    axS3 = fig.add_subplot(4, 2, 3)
+    axS3.plot(hPlant[:, 0], 'b-', label = "Fraco")
+    axS3.plot(hPlant[:, 1], 'g-', label = "Médio")
+    axS3.plot(hPlant[:, 2], 'r-', label = "Forte")
+    axS3.set_title('População de Plantas por Nível')
+    axS3.set_ylabel('%')
+    axS3.set_xlabel('Número da Iteração')
+    plt.legend()
+    axS4 = fig.add_subplot(4, 2, 5)
+    axS4.plot(hHerb[:, 0], 'b-', label = "Fraco")
+    axS4.plot(hHerb[:, 1], 'g-', label = "Médio")
+    axS4.plot(hHerb[:, 2], 'r-', label = "Forte")
+    axS4.set_title('População de Herbívoros por Nível')
+    axS4.set_ylabel('%')
+    axS4.set_xlabel('Número da Iteração')
+    plt.legend()
+    axS5 = fig.add_subplot(4, 2, 7)
+    axS5.plot(hCarn[:, 0], 'b-', label = "Fraco")
+    axS5.plot(hCarn[:, 1], 'g-', label = "Médio")
+    axS5.plot(hCarn[:, 2], 'r-', label = "Forte")
+    axS5.set_title('População de Carnívoros por Nível')
+    axS5.set_ylabel('%')
+    axS5.set_xlabel('Número da Iteração')
+    plt.legend()
+    axS2 = fig.add_subplot(4, 2, 2)
+    axS2.plot(nPlant2, 'b-', label = "Plantas")
+    axS2.plot(nHerb2, 'g-', label = "Herbívoros")
+    axS2.set_title('Simulação s/ Carnívoros')
+    axS2.set_ylabel('%')
+    axS2.set_xlabel('Número da Iteração')
+    plt.legend()
+    axS6 = fig.add_subplot(4, 2, 4)
+    axS6.plot(hPlant2[:, 0], 'b-', label = "Fraco")
+    axS6.plot(hPlant2[:, 1], 'g-', label = "Médio")
+    axS6.plot(hPlant2[:, 2], 'r-', label = "Forte")
+    axS6.set_title('População de Plantas por Nível')
+    axS6.set_ylabel('%')
+    axS6.set_xlabel('Número da Iteração')
+    plt.legend()
+    axS7 = fig.add_subplot(4, 2, 6)
+    axS7.plot(hHerb2[:, 0], 'b-', label = "Fraco")
+    axS7.plot(hHerb2[:, 1], 'g-', label = "Médio")
+    axS7.plot(hHerb2[:, 2], 'r-', label = "Forte")
+    axS7.set_title('População de Herbívoros por Nível')
+    axS7.set_ylabel('%')
+    axS7.set_xlabel('Número da Iteração')
+    plt.legend()
+    fig.set_size_inches(12, 6)
         
-    return tudo, tudo2, ecossistemas[252:], ecossistemas2[252:]
+    return tudo, tudo2, ecossistemas[(nIterations + 1):], ecossistemas2[(nIterations + 1):]
 
 #%%
 
-tudo = circleOfLife(25, 25, 250)
+tudo = circleOfLife(50, 50, 500)
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(1, 2, 1)
 ims = []
 for i in range(len(tudo[2])):
-    im = ax1.imshow(tudo[2][i], animated = True, vmin = 0, vmax = 3)
+    im = ax1.imshow(tudo[2][i], animated = True, aspect = 'auto', vmin = 1, vmax = 3)
     if i == 0:
-        ax1.imshow(tudo[2][i])
+        ax1.imshow(tudo[2][i], aspect = 'auto', vmin = 1, vmax = 3)
     ims.append([im])
 ani = animation.ArtistAnimation(fig1, ims, interval = 100, blit = True, repeat_delay = 1000)
 plt.colorbar(im)
@@ -527,10 +514,11 @@ plt.colorbar(im)
 ax2 = fig1.add_subplot(1, 2, 2)
 ims2 = []
 for i in range(len(tudo[3])):
-    im2 = ax2.imshow(tudo[3][i], animated = True, vmin = 0, vmax = 3)
+    im2 = ax2.imshow(tudo[3][i], animated = True, aspect = 'auto', vmin = 1, vmax = 3)
     if i == 0:
-        ax2.imshow(tudo[3][i])
+        ax2.imshow(tudo[3][i], aspect = 'auto', vmin = 1, vmax = 3)
     ims2.append([im2])
 ani2 = animation.ArtistAnimation(fig1, ims2, interval = 100, blit = True, repeat_delay = 1000)
 plt.colorbar(im2)
+
 plt.show()
