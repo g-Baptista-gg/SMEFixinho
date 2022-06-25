9aad32abdb2a3bff67e1081c6ae07f1a7b96092f
José Grilo <josegrilo.q1b@gmail.com>
2022-06-25 13:50:36 +0100

    Segunda Mola a Funcionar

--- SimulaçãoEModelação/Mola/mola.py
+++ SimulaçãoEModelação/Mola/mola.py
@@ -1,58 +1,73 @@
+"""
+Movimento Oscilatório Composto - Gonçalo Baptista, nº 55069; José Grilo, nº 54926
+
+Objetivo de Programação:
+"""
+#%%
+
+%matplotlib qt
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#%%
 
 class Body:
-    def __init__(self, mass, k,xEq, x0, v0, size):
+    def __init__(self, mass, k, xEq, x0, v0, size):
         self.mass = mass #  body mass
         self.k = k  #   spring constant
-        self.xEq=xEq
+        self.xEq = xEq
         self.x = x0 #   instant pos
         self.v = v0 #   instant velocity
         
         self.xList = np.zeros(size)
-        self.xList[0]=x0
+        self.xList[0] = x0
         
         self.vList = np.zeros(size)
-        self.vList[0]=v0
+        self.vList[0] = v0
    
+#%%
         
-def initialize(Tmax,dt,tSample):
-    size= int(Tmax/tSample)+1
-    nStep=int(Tmax/dt)
-    saveSteps= int (tSample/dt)
+def initialize(Tmax, dt, tSample):
+    size = int(Tmax/tSample) + 1
+    nStep = int(Tmax/dt)
+    saveSteps = int(tSample/dt)
     
-    springArray=np.zeros(2,dtype=object)
-    springArray[0]=Body(7,7,7,0,0,0)
-    springArray[1]=Body(1,1,0.5,1,0,size)
-    springArray[2]=Body(1,1,0.5,2,0,size)
+    springArray = np.zeros(2, dtype = object)
+    springArray[0] = Body(1, 10, 5, 7, 0, size)
+    springArray[1] = Body(1, 10, 5, 12, 0, size)
     
+    sForce = np.zeros(3, dtype = float)
+    sAcce = np.zeros(2, dtype = float)
+    energy = np.zeros(size, dtype = float)
     
-    springCalc(molinha,size,dt)
-    
-    return molinha
+    for i in range(size):
+        springCalc(springArray, sForce, sAcce, size, dt)
+        for j in range(2):
+            springArray[j].v += sAcce[j] * dt
+            springArray[j].vList[i] = springArray[j].v
+            springArray[j].x += springArray[j].v * dt
+            springArray[j].xList[i] = springArray[j].x
+        energy[i] = 0.5 * springArray[0].mass * springArray[0].v ** 2 + 0.5 * springArray[1].mass * springArray[1].v ** 2 + 0.5 * springArray[0].k * (springArray[0].x - springArray[0].xEq) ** 2 + 0.5 * springArray[1].k * (springArray[1].x - springArray[0].x - springArray[1].xEq) ** 2
+        
+    return springArray, energy
 
+#%%
 
-def springCalc(springArray,saveSteps,dt):
-    n=springArray.size
+def springCalc(springArray, springForce, sAcce, saveSteps, dt):
+    n = springArray.size
     for i in range(n):
-        j=i+1
-        if j != n-1:
-            a=-springArray[j].k*(springArray[j].x-(springArray[j-1].x+springArray[j].xEq))+
-            
-    
-    
-# =============================================================================
-#     for i in range (saveSteps):
-#         a=-springs.k * (springs.x-springs.xEq)
-#         springs.v += a*dt
-#         springs.x += springs.v*dt
-#         springs.xList[i]=springs.x
-#         
-# =============================================================================
+        if i == 0:
+            springForce[i] = springArray[i].k * (springArray[i].x - springArray[i].xEq)
+        else:
+            springForce[i] = springArray[i].k * (springArray[i].x - springArray[i - 1].x - springArray[i].xEq)
+    for i in range(n):
+        sAcce[i] = - springForce[i] / springArray[i].mass + springForce[i + 1] / springArray[i].mass
 
+#%%
 
+a, b = initialize(1000, 0.001, 0.005)
+plt.plot(a[0].xList)
+plt.plot(a[1].xList)
 
-a=initialize(1000,0.01,0.1)
-plt.plot(a.xList)
+fig, ax = plt.subplots()
+ax.plot(b)
